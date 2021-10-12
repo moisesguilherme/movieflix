e47a7995ab2ddd1513275e36e06e1263df6efcc9
Moises Guilherme <moisescomp@gmail.com>
2021-10-12 18:39:02 -0300

    Add Services

--- backend/src/main/java/com/devsuperior/movieflix/services/GenreService.java
+++ backend/src/main/java/com/devsuperior/movieflix/services/GenreService.java
@@ -1,14 +1,22 @@
 package com.devsuperior.movieflix.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.movieflix.dto.GenreDTO;
 import com.devsuperior.movieflix.entities.Genre;
 import com.devsuperior.movieflix.repositories.GenreRepository;
+import com.devsuperior.movieflix.services.exceptions.DatabaseException;
+import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class GenreService {
@@ -19,6 +27,46 @@ public class GenreService {
 	public List<GenreDTO> findAll() {
 		List<Genre> list = repository.findAll(); 
 		return list.stream().map( x -> new GenreDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional(readOnly = true)
+	public GenreDTO findById(Long id) {
+		Optional<Genre> obj = repository.findById(id);
+		Genre entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));		
+		return new GenreDTO(entity);
+	}
+
+	@Transactional
+	public GenreDTO insert(GenreDTO dto) {
+		Genre entity = new Genre();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);		
+		return new GenreDTO(entity);
+	}
+	
+	@Transactional
+	public GenreDTO update(Long id, GenreDTO dto) {
+
+		try {	
+			Genre entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity = repository.save(entity);		
+			return new GenreDTO(entity);
+	   }catch(EntityNotFoundException e) {
+		   throw new ResourceNotFoundException("Id not found" + id);
+	   }	  
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}		
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation"); 
+		}
 	}
 
 }
