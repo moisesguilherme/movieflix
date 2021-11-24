89496069d97047392e0584230016ddc0ac9499a6
Moises Guilherme <moisescomp@gmail.com>
2021-11-24 17:25:13 -0300

    Finish home page

--- frontend-web/src/pages/Auth/components/Login/index.tsx
+++ frontend-web/src/pages/Auth/components/Login/index.tsx
@@ -8,16 +8,16 @@ const Login = () => {
             <h1 className="login-text">LOGIN</h1>
             <form className="login-form">
                 <input
-                    className="form-control input-base"
+                    className="input-base"
                     type="email"
                     placeholder="Email"
                 />
                 <input
-                    className="form-control input-base"
+                    className="input-base"
                     type="password"
-                    placeholder="senha"
+                    placeholder="Senha"
                 />
-                <div className="login-submit">
+                <div className="login-submit login-submit-text">
                     FAZER LOGIN
                 </div>
             </form>
