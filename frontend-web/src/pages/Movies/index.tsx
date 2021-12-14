99eb3fffc93015e2ed04c35b9a86b70d61a280e7
Moises Guilherme <moisescomp@gmail.com>
2021-12-14 20:48:29 -0300

    Add pagination

--- frontend-web/src/pages/Movies/index.tsx
+++ frontend-web/src/pages/Movies/index.tsx
@@ -4,6 +4,7 @@ import { Movie, MoviesResponse } from 'core/types/movie';
 import MovieCard from './components/MovieCard';
 import MovieCardLoader from './components/Loaders/MovieCardLoader';
 import { makePrivateRequest } from 'core/utils/request';
+import Pagination from 'core/components/Pagination';
 
 import './styles.scss';
 
@@ -11,43 +12,53 @@ type Props = {
     text: string;
 }
 
-const Movies = ( {text } : Props) => {
-    
+const Movies = ({ text }: Props) => {
+
     const [moviesResponse, setMoviesResponse] = useState<MoviesResponse>();
     const [isLoading, setIsLoading] = useState(false);
-    
+    const [activePage, setActivePage] = useState(0);
+
     const getMovies = useCallback(() => {
         const params = {
-            page: 0, 
+            page: activePage,
             linesPerPage: 4,
             orderBy: 'id'
         }
 
         setIsLoading(true);
-        makePrivateRequest({ url: '/movies', params})
+        makePrivateRequest({ url: '/movies', params })
             .then(response => setMoviesResponse(response.data))
             .finally(() => {
                 setIsLoading(false);
-                
+
             })
-    }, [])
-    
+    }, [activePage])
+
     useEffect(() => {
         getMovies();
     }, [getMovies]);
-    
+
     return (
-        <>
-                 <div className="movies-container">
-                    {isLoading ? <MovieCardLoader/> : (
-                        moviesResponse?.content.map(movie => (
-                            <Link to={`/movies/${movie.id}`} key={movie.id}>
-                                <MovieCard movie={movie}/>
-                            </Link>
-                        ))
-                    )}
-                </div> 
-        </>
+        <div className="movies-container">
+            <div className="movies-card-container">
+                {isLoading ? <MovieCardLoader /> : (
+                    moviesResponse?.content.map(movie => (
+                        <Link to={`/movies/${movie.id}`} key={movie.id}>
+                            <MovieCard movie={movie} />
+                        </Link>
+                    ))
+                )}
+            </div>
+
+            {moviesResponse && (
+                    <Pagination
+                        totalPages={moviesResponse.totalPages}
+                        activePage={activePage}
+                        onChange={page => setActivePage(page)}
+                    />
+                )}
+
+        </div>
     )
 }
 
